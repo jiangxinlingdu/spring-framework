8e5cd22c0c51b94555ddf8e27f8e6e975b0af6d4
lingdu <xxxx@qq.com>
2020-09-12 21:13:15 +0800

    初始化准备

--- spring-lingdu/src/main/java/com/lingdu/test/Test1.java
+++ spring-lingdu/src/main/java/com/lingdu/test/Test1.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test1 {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(AppTest.class);
-		annotationConfigApplicationContext.getBean(TestBean.class);
+		System.out.println(annotationConfigApplicationContext.getBean(TestBean.class));
 	}
 }
