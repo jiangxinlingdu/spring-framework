7c4f956aead2b9ebc1db78b701999ece6d985450
lingdu <xxxx@qq.com>
2020-10-20 14:26:40 +0800

    'm'

--- spring-core/src/main/java/org/springframework/core/env/StandardEnvironment.java
+++ spring-core/src/main/java/org/springframework/core/env/StandardEnvironment.java
@@ -75,8 +75,10 @@ public class StandardEnvironment extends AbstractEnvironment {
 	 */
 	@Override
 	protected void customizePropertySources(MutablePropertySources propertySources) {
+		//系统属性
 		propertySources.addLast(
 				new PropertiesPropertySource(SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME, getSystemProperties()));
+		//环境变量属性
 		propertySources.addLast(
 				new SystemEnvironmentPropertySource(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, getSystemEnvironment()));
 	}
